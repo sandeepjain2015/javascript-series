16d740a8a01a183fb557f8e043b4a8d7680c1fe7
sandeep jain <mr.sandeepmcscet@gmail.com>
2024-04-05 10:05:40 +0530

    first commit

---
+++ dataType.js
@@ -0,0 +1,32 @@
+// Primitive
+// String 
+// Number 
+// Bollean
+// null
+// undefined
+// BigInt
+// Symbol
+
+// Non Primitive
+// Array
+// Object
+
+
+
+let name = 'ram'
+
+let number = 1
+let isLogged = true
+
+const users = ['ram','shyam']
+let user  = {
+    name : 'ram',
+    age:50,
+    "full name" :"ram jain"
+}
+
+console.log(user["full name"]);
+
+let newNum = true+1+2+4
+console.log(newNum);
+console.log(typeof(newNum));
---
+++ variable.js
@@ -0,0 +1,16 @@
+// var 
+// let 
+// const 
+function abc(){
+    let a=10
+const b=30
+// const b=40
+if(true) {
+   a=20
+   const b=40
+   console.log(a);
+} 
+console.log(a);
+console.log(b);
+}
+abc()
